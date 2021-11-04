e808f28b2d4029735a4bbf6d021022101bc2c05a
LuckLin520 <502763576@qq.com>
2021-11-04 17:53:51 +0800

    publish 1.0.0

---
+++ components/index.js
@@ -0,0 +1,1 @@
+export { default } from './zhilin-cascader/index.vue'
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="content">
+		<zhilin-cascader v-model="show" :options="options" @confirm="onConfirm"></zhilin-cascader>
+	</view>
+</template>
+
+<script>
+	const options = [{
+			value: 'zhejiang',
+			label: 'Zhejiang',
+			children: [{
+				value: 'hangzhou',
+				label: 'Hangzhou',
+				children: [{
+					value: 'xihu',
+					label: 'West Lake',
+				}]
+			}]
+		},
+		{
+			value: 'jiangsu',
+			label: 'Jiangsu',
+			children: [{
+				value: 'nanjing',
+				label: 'Nanjing',
+				children: [{
+					value: 'zhonghuamen',
+					label: 'Zhong Hua Men',
+				}]
+			}]
+		}
+	]
+	export default {
+		data() {
+			return {
+				options,
+				show: true
+			}
+		},
+		methods: {
+			onConfirm(value) {
+				console.log(value)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+</style>
